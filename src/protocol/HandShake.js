65db054e1eabd51ef1fe7335ee3f6aa7d6cb0878
yisbug <yisbug@gmail.com>
2022-10-12 04:38:21 +0800

    feat: startHandshakeIK

--- src/protocol/HandShake.js
+++ src/protocol/HandShake.js
@@ -2,6 +2,7 @@ const CipherState = require('./CipherState');
 const WASymmetricState = require('./WASymmetricState');
 const HandshakeState = require('./HandshakeState');
 const SwitchableHandshakeState = require('./SwitchableHandshakeState');
+const IKHandshakePattern = require('./handshakepatterns/IKHandshakePattern');
 
 class HandShake {
   constructor(waSocketClient) {
@@ -15,11 +16,56 @@ class HandShake {
     this._prologue = Buffer.concat([Buffer.from('WA'), waversion]);
 
     this.EDGE_HEADER = Buffer.concat([Buffer.from('ED'), Buffer.from([0, 1])]);
+
+    const cipherState = new CipherState('AESGCM');
+    const waSymmetricState = new WASymmetricState(cipherState);
+    const handshakeState = new SwitchableHandshakeState(new HandshakeState(waSymmetricState));
+    this.handshakeState = handshakeState;
   }
 
-  async login() {
-    const waSymmetricState = new WASymmetricState(new CipherState('AESGCM'));
-    const handshakeState = new SwitchableHandshakeState(new HandshakeState(waSymmetricState));
+  async login(config, serverStaticPublic) {
+    let clientStaticKeypair = Buffer.from(config.client_static_keypair, 'base64');
+    clientStaticKeypair = {
+      private: clientStaticKeypair.slice(0, 32),
+      public: clientStaticKeypair.slice(-32),
+    };
+
+    let cipherstatepair;
+    if (serverStaticPublic) {
+      try {
+        console.debug('perform use startHandshakeIK');
+        cipherstatepair = await this.startHandshakeIK(
+          config,
+          clientStaticKeypair,
+          serverStaticPublic
+        );
+      } catch (e) {
+        console.error('perform use startHandshakeIK error', e);
+        console.debug('perform use switchHandshakeXXFallback');
+        cipherstatepair = await this.switchHandshakeXXFallback(
+          clientStaticKeypair,
+          this.createPayload(config),
+          e.serverHello
+        );
+      }
+    } else {
+      console.debug('perform use startHandshakeXX');
+      cipherstatepair = await this.startHandshakeXX(config, clientStaticKeypair);
+    }
+    console.info('Handshake success.');
+    return cipherstatepair;
+  }
+
+  async startHandshakeIK(config, clientStaticKeypair, serverStaticPublic) {
+    const ik = new IKHandshakePattern();
+    this.handshakeState.initialize(
+      ik,
+      true,
+      this._prologue,
+      clientStaticKeypair,
+      null,
+      serverStaticPublic
+    );
   }
 }
 
