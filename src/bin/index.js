af1b107f04ec2a8424783e2e4c73ea3bc50d5ca0
yisbug <yisbug@gmail.com>
2022-11-02 10:01:56 +0800

    fix: deps

--- src/bin/index.js
+++ src/bin/index.js
@@ -5,8 +5,10 @@ const isDev = fse.existsSync(path.join(__dirname, './darwin/d0.js'));
 
 const basePath = isDev ? './darwin/' : './linux/';
 
+const files = fse.readdirSync(path.join(__dirname, './darwin')).filter(item => item[0] !== '.');
+
 const obj = {};
-for (let i = 0; i <= 6; i++) {
+for (let i = 0; i < files.length; i++) {
   // eslint-disable-next-line
   obj[`d${i}`] = require(`${basePath}d${i}`);
 }
